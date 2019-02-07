4b76cf8a961535bba9f415cef8fc0128e02c024d
Jessica Beeli <jmbeeli@gmail.com>
2019-02-06 23:17:28 -0700

    biz time edits

--- exercises/week-3/bizTime/app.js
+++ exercises/week-3/bizTime/app.js
@@ -1,5 +1,9 @@
+//////////////////////////////////////////STICKY//////////////////////////////////////////////////////////////
+
 // When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
+window.onscroll = function () {
+  myFunction()
+};
 
 // Get the navbar
 var navbar = document.getElementById("navbar");
@@ -16,12 +20,11 @@ function myFunction() {
   }
 }
 
+//////////////////////////////////////////STICKY//////////////////////////////////////////////////////////////
 
-<button onclick="one()">1</button>
-<button onclick="two()">2</button>
-<button onclick="four()">4</button>
 
-<script>
+
+//////////////////////////////////////////PHOTO GRID//////////////////////////////////////////////////////////////
 // Get the elements with class="column"
 var elements = document.getElementsByClassName("column");
 
@@ -31,21 +34,70 @@ var i;
 // Full-width images
 function one() {
   for (i = 0; i < elements.length; i++) {
-    elements[i].style.flex = "100%"; 
+    elements[i].style.flex = "100%";
   }
 }
 
 // Two images side by side
 function two() {
   for (i = 0; i < elements.length; i++) {
-    elements[i].style.flex = "50%"; 
+    elements[i].style.flex = "50%";
   }
 }
 
 // Four images side by side
 function four() {
   for (i = 0; i < elements.length; i++) {
-    elements[i].style.flex = "25%"; 
+    elements[i].style.flex = "25%";
   }
 }
-</script>
+//////////////////////////////////////////PHOTO GRID//////////////////////////////////////////////////////////////
+
+
+
+
+///////////////////////////////////FORM //////////////////////////////
+
+contact.addEventListener("submit", function (e) {
+  e.preventDefault()
+  //clear the container back to an empty div
+  container.innerHTML = " "
+  //grab the inputs value
+  // variable new name = document name.input name.value
+  var nameInputFirst = contact.first.value
+  var phoneInput = contact.phone.value
+  var serviceInput = contact.service.value
+  var finalPrint = (" Name: " + nameInputFirst + " Phone Number: " + phoneInput + " Service: " + serviceInput)
+  // // Clear the user input after grabbing the value
+  nameInputFirst.value = ""
+  phoneInput.value = ""
+  serviceInput = ""
+  //create element
+  var h1Thing = document.createElement('h1');
+  // Give the element content: Text, attributes, classes, eventlisteners
+  h1Thing.textContent = finalPrint
+  // h1Thing.classList.add("main-title")
+  // Append child
+  container.appendChild(h1Thing)
+
+
+})
+
+///////////////////FORM//////////////////////////
+
+foot.addEventListener('submit', function(e) {
+  e.preventDefault()
+  //clear container back to an empty div when done //maybe not? have a running list
+  container2.innerHTML = " "
+  // grab inputs value //variable new name = document name . input name . value
+  var nameInput =foot.firstLast.value
+  // console.log(nameInput)
+  var endPrint = ("Thank you " + nameInput + " for your support for Courtney's Foot. Please also venmo her $5000 dollars.")
+  // nameInput.value = ""
+  //create element
+  var h2Thing = document.createElement('h2')
+  //Give element content
+  h2Thing.textContent = endPrint
+  //append child
+  container2.appendChild(h2Thing)
+})
