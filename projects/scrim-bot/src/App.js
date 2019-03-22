c4b818709251364af7fce613ab76c3d14f56cecd
Jessica Beeli <jmbeeli@gmail.com>
2019-03-22 14:56:11 -0600

    SCRIM BOT WORKS

--- projects/scrim-bot/src/App.js
+++ projects/scrim-bot/src/App.js
@@ -1,29 +1,93 @@
 import React from 'react'
 import Chatkit from '@pusher/chatkit-client'
-
 import MessageList from './components/MessageList'
 import SendMessageForm from './components/SendMessageForm'
 import RoomList from './components/RoomList'
-import NewRoomForm from './components/NewRoomForm'
+// import NewRoomForm from './components/NewRoomForm'
 import './style.css'
-import { tokenUrl, instanceLocator } from './config'
 
-const myRoomId = '19394103'
+import { tokenUrl, instanceLocator } from './config'
+import UserMessageList from './components/UserMessageList';
 
 class App extends React.Component {
-
-    constructor(){
+    
+    constructor() {
         super()
         this.state = {
-            messages: []
+            roomId: '19394103',
+            messages: [
+                {
+                id: 8329749823,
+                senderId: 'SSHbot',
+                text: '1. Has your partner ever threatened violence?'
+                },
+                {
+                id: 8329749823,
+                senderId: 'SSHbot',
+                text: '2. Has your partner ever taken your cell phone?'
+                },
+                {
+                id: 8329749825,
+                senderId: 'SSHuser',
+                text: '3. Do you feel anxious or nervous when you are around your partner?'
+                },
+                {
+                    id: 8329749825,
+                    senderId: 'SSHbot',
+                    text: '4.Do you watch what you are doing in order to avoid making your partner angry or upset?' 
+                },
+                {
+                    id: 8329749825,
+                    senderId: 'SSHbot',
+                    text: '5. Do you feel obligated or coerced into having sex with your partner?' 
+                }, 
+                {
+                    id: 8329749825,
+                    senderId: 'SSHbot',
+                    text: '6. Are you afraid of voicing a different opinion than your partner?' 
+                },
+                {
+                    id: 8329749825,
+                    senderId: 'SSHbot',
+                    text: '7. Does your partner criticize you or embarrass you in front of others?' 
+                },
+                {
+                    id: 8329749825,
+                    senderId: 'SSHbot',
+                    text: '8. Does your partner check up on what you have been doing, and believe your answers?' 
+                },
+                {
+                    id: 8329749825,
+                    senderId: 'SSHbot',
+                    text: '9. Is your partner jealous, such as accusing you of having affairs?' 
+                },
+                {
+                    id: 8329749825,
+                    senderId: 'SSHbot',
+                    text: '10. Does your partner tell you that they will stop beating you when you start behaving?' 
+                }
+            ],
+            userMessages: [
+                {
+                    id: '',
+                    senderId: 'user',
+                    text: ''
+                }
+            ],
+            joinableRooms: [],
+            joinedRooms: [],
+            currentUser: {}
         }
-    }    
-
-
+        // this.sendMessage = this.sendMessage.bind(this)
+        // this.subscribeToRoom = this.subscribeToRoom.bind(this)
+        this.getRooms = this.getRooms.bind(this)
+        // this.createRoom = this.createRoom.bind(this)
+    } 
+    
     componentDidMount() {
         const chatManager = new Chatkit.ChatManager({
             instanceLocator,
-            userId: 'jmbiene', /** swap out */
+            userId: 'SSHbot',
             tokenProvider: new Chatkit.TokenProvider({
                 url: tokenUrl
             })
@@ -31,30 +95,121 @@ class App extends React.Component {
         
         chatManager.connect()
         .then(currentUser => {
-            currentUser.subscribeToRoom({
-                roomId: myRoomId, /** swap out */ 
-                ///made me make this a string, i think that might be whats breaking it
-                //parseint
-                hooks: {
-                    onNewMessage: message => {
-                        // console.log('message.text: ', message.text);
-                        this.setState({
-                            messages: [...this.state.messages, message]
-                        })
-                    }
+            this.currentUser = currentUser
+            this.getRooms()
+        })
+        .catch(err => console.log('error on connecting: ', err))
+    }
+    
+    getRooms() {
+        this.currentUser.getJoinableRooms()
+        .then(joinableRooms => {
+            this.setState({
+                joinableRooms,
+                joinedRooms: this.currentUser.rooms
+            }
+            // , () => this.testFunc(this.state.roomId) 
+            )
+        })
+        .catch(err => console.log('error on joinableRooms: ', err))
+    }
+
+    testFunc = (roomId) => {
+        console.log('getmsg ran')
+        // this.setState({ messages: [] })
+        this.currentUser.subscribeToRoom({
+            roomId: roomId,
+            hooks: {
+                onNewMessage: message => {
+                    this.setState({
+                        messages: [...this.state.messages, message]
+                    })
                 }
+            }
+        })
+        .then(room => {
+            this.setState({
+                roomId: room.id
             })
         })
     }
     
+    subscribeToRoom(roomId) {
+        console.log('subscribed')
+        this.setState({ messages: [] })
+        this.currentUser.subscribeToRoom({
+            roomId: roomId,
+            hooks: {
+                onNewMessage: message => {
+                    console.log(message)
+                    this.setState({
+                        messages: [...this.state.messages, message]
+                    })
+                }
+            }
+        })
+        .then(room => {
+            this.setState({
+                roomId: room.id
+            })
+            this.getRooms()
+        })
+        .catch(err => console.log('error on subscribing to room: ', err))
+    }
+    
+    sendMessage = (text) => {
+        console.log(text)
+        let userInputMsg = {
+            id: 8329749826,
+            senderId: 'User',
+            text: text
+            }
+            
+        this.setState(prevState => ({
+            userMessages: [
+                ...prevState.userMessages, userInputMsg
+            ]
+        }))
+        // this.setState({
+        //     userMessages: [userInputMsg]
+            
+        // })
+        // this.currentUser.sendMessage({
+        //     text,
+        //     roomId: this.state.roomId
+        // })
+    }
+
+
+    
+    // createRoom(name) {
+    //     console.log('create room ran')
+    //     this.currentUser.createRoom({
+    //         name
+    //     })
+    //     .then(room => this.subscribeToRoom(room.id))
+    //     .catch(err => console.log('error with createRoom: ', err))
+    // }
+    
     render() {
-        console.log('this.state.messages:', this.state.messages)
+        console.log(this.state.roomId)
         return (
             <div className="app">
-                <RoomList />
-                <MessageList messages={this.state.messages}/>
-                <SendMessageForm />
-                <NewRoomForm />
+                <RoomList
+                    subscribeToRoom={this.subscribeToRoom}
+                    rooms={[...this.state.joinableRooms, ...this.state.joinedRooms]}
+                    roomId={this.state.roomId} />
+                <MessageList 
+                    roomId={this.state.roomId}
+                    messages={this.state.messages} 
+                    userMessages={this.state.userMessages}
+                    />
+                <SendMessageForm
+                    disabled={!this.state.roomId}
+                    sendMessage={this.sendMessage} />
+                {/* <UserMessageList 
+                    userMessages={this.state.userMessages}/> */}
+                {/* <NewRoomForm createRoom={this.createRoom} /> */}
             </div>
         );
     }
--- projects/scrim-bot/src/components/MessageList.js
+++ projects/scrim-bot/src/components/MessageList.js
@@ -1,15 +1,57 @@
 import React from 'react'
-import Message from'./Message'
+import ReactDOM from 'react-dom'
+import Message from './Message'
+import  UserMessage from './UserMessage'
 
 class MessageList extends React.Component {
+    
+    componentWillUpdate() {
+        const node = ReactDOM.findDOMNode(this)
+        this.shouldScrollToBottom = node.scrollTop + node.clientHeight + 100 >= node.scrollHeight
+    }
+    
+    componentDidUpdate() {
+        console.log('updatedd')
+        if (this.shouldScrollToBottom) {
+            const node = ReactDOM.findDOMNode(this)
+            node.scrollTop = node.scrollHeight   
+        }
+        // this.test()
+    }
+    test = () => {
+        const dataArr = []
+        console.log(this.props.userMessages.length + "hi")
+        for(let i = 0; i < this.props.userMessages.length; i++) {
+            console.log('i is' +i)
+            if (this.props.userMessages.length && this.props.userMessages.length > 10){
+                dataArr.push(
+                    <div>
+                        <Message key={this.props.messages[i].id} username={this.props.messages[i].senderId} text={this.props.messages[i].text} /> 
+                        <UserMessage key={this.props.userMessages[i].id} username={this.props.userMessages[i].senderID} text={this.props.userMessages[i].text} />
+                    </div>
+                )
+            } 
+           
+    }
+    return dataArr
+}
+
     render() {
         return (
+            
             <div className="message-list">
-                {this.props.messages.map((message, index) => {
+                {/* {this.props.messages.map((message, index) => {
                     return (
-                        <Message key={index} username={message.senderId} text={message.text}/>
+                        <Message key={message.id} username={message.senderId} text={message.text} />
                     )
                 })}
+                { this.props.userMessages.map(userMessages => {
+                    return (
+                        <UserMessage key={userMessages.id} username={userMessages.senderID} text={userMessages.text} />
+                        
+                    )
+                })} */}
+                {this.test()}
             </div>
         )
     }
