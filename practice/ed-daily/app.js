515fa8230a38078acdc2518e13a32e3ca46af40f
Jessica Beeli <jmbeeli@gmail.com>
2019-04-16 09:09:44 -0700

    edabits

--- practice/ed-daily/app.js
+++ practice/ed-daily/app.js
@@ -4,3 +4,39 @@ const asciiConvert = (c) => {
 
 
 console.log(asciiConvert('A m / [ '))
+
+
+
+// function numberSyllables(word) {
+//     let splitted = word.split('')
+//     let count = 1
+//     if(splitted.charCodeAt(1)){
+//         return count++
+//     } 
+// }
+
+
+// console.log(numberSyllables('buf-fet'))
+
+
+
+function missingAngle(angle1, angle2) {
+    let sum = angle1 + angle2
+    let angle3 = 180 - sum
+    if ( angle3 > 90){
+        return 'obtuse'
+    } else if (angle3 === 90){
+        return 'right'
+    } else if (angle3 < 90){
+        return 'acute'
+    }
+}
+
+
+console.log(missingAngle(45, 45))
+
+const syllaCount = (word) => {
+    return word.split('-').length
+}
+
+console.log(syllaCount('on-o-mat-o-poe-ia'))
