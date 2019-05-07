1efa76bcc5c94bfbcd62d1bdab936a97e1624caf
Jessica Beeli <jmbeeli@gmail.com>
2019-05-06 17:34:59 -0700

    edabits daily

--- practice/ed-daily/app.js
+++ practice/ed-daily/app.js
@@ -140,6 +140,7 @@ const checkElement  = ( arr, el) => {
 }
 console.log(checkElement([1, 2, 3, 4, 5], 3))
 
+
 //existsHigher([5, 3, 15, 22, 4], 10) ➞ true
 const existsHigher = (arr, el) => {
     if (arr.some()){
